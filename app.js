de4e4810f8909d2185193f3d9dd14c868086e21d
leaf_hsiao <leaf_hsiao@outlook.com>
2019-03-06 21:20:40 -0500

    ical

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var weatherRouter = require('./routes/weather')
+var icalRouter = require('./routes/ical')
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/weather', weatherRouter);
+app.use('/ical', icalRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
