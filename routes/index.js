b81c2d9c397c5b97864c48a58f0e83feec413236
leaf-hsiao <leaf_hsiao@outlook.com>
2019-03-10 19:38:43 -0400

    json format ical

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,19 @@ let url    = 'http://api.openweathermap.org/data/2.5/weather?q='
 let appId  = 'appid=57035ca996576e3a8950a1c40ff181e5';
 let units  = '&units=metric'; 
 var request = require('request');
+var ical = require('node-ical');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
- res.render('index', {'body':'', forecast: ''});
+  res.render('index', {'body':'', forecast: ''});
 });
 
 router.post('/weather', function(req, res, next){
   let city = req.body.city;
   url = url+city+"&"+appId;
 
- request(url, function (error, response, body) {
+  request(url, function (error, response, body) {
       console.log('error:', error); // Print the error if one occurred
       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
       body = JSON.parse(body);
@@ -30,6 +31,7 @@ router.post('/weather', function(req, res, next){
     let forecast = "For city "+city+', country '+country;
 
     res.render('index', {body : body, forecast: forecast});
-   });
+  });
 });
+
 module.exports = router;
