5d93c2b934b095bed0edc1918b8ca5fd3d595f1e
Yuan <byrd.zhang@gmail.com>
2019-04-21 15:37:07 -0400

    Some little changes without changing any function

--- routes/ical.js
+++ routes/ical.js
@@ -6,7 +6,6 @@ var ical = require('node-ical');
 router.get('/', function (req, res) {
   ical.fromURL('https://sit.instructure.com/feeds/calendars/user_TvB3EwF9kzFKDoNOVqAr9z8kJU8F1tIRvPFK9iqW.ics', {}, function (err, data) {
     if (err) console.log(err);
-    //console.log(1);
     res.json(data);
   });
 });
