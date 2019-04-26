8014689045885ce6100ecbbe53e9207148d7e6a0
Vigzm1995 <vigmohan95@gmail.com>
2019-04-26 10:19:40 -0400

    Added email functionality after user registration.

--- app.js
+++ app.js
@@ -7,11 +7,83 @@ const expressValidator = require('express-validator');
 const session = require('express-session');
 const passport = require('passport');
 
+const nodemailer = require('nodemailer');
+
+// Init App
+const app = express();
+
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/views', (req, res) => {
+    res.render('login');
+});
+
+
+//Returned in the terminal.
+app.post('/send', (req, res) => {
+
+    console.log(req.body);
+    
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: 'noreplyduckmommy@gmail.com', // generated ethereal user
+        pass: 'noreplyduckmommy1234'  // generated ethereal password
+    },
+    tls:{
+      rejectUnauthorized:false
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: '"Duck Mommy Test" <noreplyduckmommy.com>', // sender address
+      to: req.body.email, // list of receivers
+      subject: 'Duck Mommy Test', // Subject line
+      text: 'Welcome to DuckMommy. This is a test mail from Duck Mommy. Thank you for using our services. Have a great Day! DuckMommy Team!', // plain text body  
+    };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);   
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+      res.render('login', {msg:'Email has been sent'});
+  });
+});
+
+// Express Session Middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+}));
+
 /*
 mongoose.connect('mongodb://localhost/DuckMommyDB', {
     useNewUrlParser: true
 });
 */
+
+/*Database Connection*/
+
 const config = require('./config/database');
 mongoose.connect(config.database, {
     useNewUrlParser: true
@@ -27,33 +99,7 @@ db.once('open', () => {
 // Check for DB errors
 db.on('error', (err) => {
     console.log(err)
-});
-
-// Init App
-const app = express();
-
-// Load View Engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// Static files
-app.use(express.static(path.join(__dirname, 'public')));
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
-
-// parse application/json
-app.use(bodyParser.json())
-
-
-// Express Session Middleware
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-}));
+})
 
 // Express Messages Middleware
 app.use(require('connect-flash')());
