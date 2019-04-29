68dfac8aeac441bb438a4b10357451a3f208fd1a
Yuan <byrd.zhang@gmail.com>
2019-04-29 06:07:58 -0400

    Update the frontend(but don't merge)
    Modules doesn't work!

--- public/js/script.js
+++ public/js/script.js
@@ -25,12 +25,12 @@ $.get("/ical", function (data, status) {
     var todolist = "";
     var count = 3;
     $.each(data, function (i) {
-        var date_src = data[i].end.slice(0, 10).replace(/-/g,"/");
+        var date_src = data[i].end.slice(0, 10).replace(/-/g, "/");
         var date_obj = new Date(date_src);
-        var preDate = new Date(today.getTime() - 24*60*60*1000);
+        var preDate = new Date(today.getTime() - 24 * 60 * 60 * 1000);
         if (date_obj >= preDate) {
             if (count > 0) {
-                var description = data[i].description.replace(/\r\n/g,"<br>").replace(/\n/g,"<br>");
+                var description = data[i].description.replace(/\r\n/g, "<br>").replace(/\n/g, "<br>");
                 icaldetails = '<ul><li class="hw-date">' + data[i].end.slice(0, 10) + '</li><li class="hw-sum">' + data[i].summary + '</li><li class="hw-des">' + description + "</li></ul>";
                 $(".ical").append(icaldetails)
                 todolist = "<ul><li>" + data[i].end.slice(0, 10) + "</li><li>" + data[i].summary + "</li></ul>";
